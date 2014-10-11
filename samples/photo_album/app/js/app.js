5def4dfc1f7acafd696f8ae8d2438e7b32ff9f75
Valentin Waeselynck <val@bandsquare.fr>
2014-10-12 00:55:43 +0200

    Removed global variables + Cloudinary config angular module with constants

--- samples/photo_album/app/js/app.js
+++ samples/photo_album/app/js/app.js
@@ -1,42 +1,46 @@
 'use strict';
+(function () {
 
-/* App Module */
-var photoAlbumApp = angular.module('photoAlbumApp', [
-  'ngRoute',
-  'cloudinary',
-  'photoAlbumAnimations',
-  'photoAlbumControllers',
-  'photoAlbumServices'
-]);
+  /* App Module */
+  var photoAlbumApp = angular.module('photoAlbumApp', [
+    'ngRoute',
+    'cloudinary',
+    'photoAlbumAnimations',
+    'photoAlbumControllers',
+    'photoAlbumServices',
+    'photoAlbumCloudinaryConfig'
+  ]);
 
-photoAlbumApp.config(['$routeProvider',
-  function($routeProvider) {    
-    $routeProvider.
-      when('/photos', {
-        templateUrl: 'partials/photo-list.html',
-        resolve: {
-          photoList: function($q, $rootScope, album) {
-            if (!$rootScope.serviceCalled) {
-              return album.photos({}, function(v){
-                $rootScope.serviceCalled = true;
-                $rootScope.photos = v.resources;
-              });
-            } else {
-              return $q.when(true);
+  photoAlbumApp.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/photos', {
+          templateUrl: 'partials/photo-list.html',
+          resolve: {
+            photoList: function($q, $rootScope, album) {
+              if (!$rootScope.serviceCalled) {
+                return album.photos({}, function(v){
+                  $rootScope.serviceCalled = true;
+                  $rootScope.photos = v.resources;
+                });
+              } else {
+                return $q.when(true);
+              }
             }
           }
-        }
-      }).
-      when('/photos/new', {
-        templateUrl: 'partials/photo-upload.html',
-        controller: 'photoUploadCtrl'
-      }).
-      when('/photos/new_jquery', {
-        templateUrl: 'partials/photo-upload-jquery.html',
-        controller: 'photoUploadCtrlJQuery'
-      }).
-      otherwise({
-        redirectTo: '/photos'
-      });
-  }]);
+        }).
+        when('/photos/new', {
+          templateUrl: 'partials/photo-upload.html',
+          controller: 'photoUploadCtrl'
+        }).
+        when('/photos/new_jquery', {
+          templateUrl: 'partials/photo-upload-jquery.html',
+          controller: 'photoUploadCtrlJQuery'
+        }).
+        otherwise({
+          redirectTo: '/photos'
+        });
+    }]);
+
+}());
 
